6b0f4a740559b744f5ced833a26fce2be1594f00
minanurrohman223 <41333729+minanurrohman223@users.noreply.github.com>
2018-07-17 20:24:46 +0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ var app = {
     // Application Constructor
     initialize: function() {
     
-        var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+        var url = "https://api.nasa.gov/planetary/apod?api_key=PCJU0dLpyY9ItmQIwdLxyXVAPnKgSoqQOscGBi9k";
         
         $("#loadImage").click(function(){
             $.ajax({
